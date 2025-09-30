8dca14e74efcab267177d23a2d13b435f24b8180
Cruzer-S <yyyynoom@gmail.com>
2025-10-01 06:57:57 +0900

    show map and player (temp)

--- source/main.c
+++ source/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "player.h"
+#include "image.h"
 
-Player create_player(void)
+#include <ncurses.h>
+
+static Player create_player(void)
 {
 	Player player;
 
@@ -23,6 +27,39 @@ Player create_player(void)
 	return player;
 }
 
+static void draw_map(void)
+{
+	static enum image map[10][10] = {
+		{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 1, 1, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 1, 1, 1 },
+		{ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 1, 0, 0, 1, 1, 0, 0, 0 },
+		{ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
+		{ 0, 0, 1, 0, 0, 0, 0, 0, 1, 0 },
+	};
+
+	for (int y = 0; y < 10; y++)
+		for (int x = 0; x < 10; x++)
+			mvaddch(y, x, img2chr[map[y][x]]);
+}
+
+static void draw_object(Object obj)
+{
+	int x, y;
+	enum image image;
+
+	x = object_get_x(obj);
+	y = object_get_y(obj);
+
+	image = object_get_image(obj);
+
+	mvaddch(y, x, img2chr[image]);
+}
+
 int main(void)
 {
 	Player player;
@@ -34,7 +71,20 @@ int main(void)
 	}
 
 	printf("name: %s\n", player_get_name(player));
-	printf("level: %d\n", player_get_level(player));	
+	printf("level: %d\n", player_get_level(player));
+
+	object_set_xy(player_as_object(player), 2, 5);
+
+	initscr();
+
+	do {
+		draw_map();
+		draw_object(player_as_object(player));
+	} while (getch());
+
+	player_destroy(player);
+
+	endwin();
 
 	return 0;
 }
